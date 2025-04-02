cdf10b0b7753af1a1e5a7f25aade533902c35c6a
Vincent Petkevicius <vincas100@gmail.com>
2025-04-02 20:27:46 +0300

    Component templates

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,46 @@
+import React, { useState } from 'react';
+import SearchBox from './SearchBox';
+import WeathetDisplay from './WeatherDisplay';
+import FiveDayForcast from './FiveDayForecast';
+
 function App() {
-  return <h1>Weather Forecast App</h1>;
+  const [search, setSearch] = useState('');
+
+  // ??? placeholder
+  const handleSearch = () => {
+    console.log(`Searching for: ${search}`);
+    // Add your search logic here (e.g., API call)
+  };
+  // This function is called when a button is clicked
+  const handleButtonClick = (city) => {
+    setSearch(city);
+    handleSearch();
+  };
+
+  //Hardcoded weather data for demonstration purposes
+const sampleWeatherData = {
+  main: {
+    temp: 25,
+    humidity: 60
+  },
+  weather: [
+    {
+      description: "clear sky"
+    }
+  ],
+  wind: {
+    speed: 5
+  }
+};
+
+  return (
+  <div> 
+    <h1>Weather Forecast App</h1>
+    <h2>Search Box test</h2>
+    <SearchBox search={search} setSearch={setSearch} handleSearch={handleSearch} />
+    <WeathetDisplay weatherData={sampleWeatherData} />
+    <FiveDayForcast FiveDayForcast />
+  </div>
+  );
 }
 export default App;
---
+++ src/FiveDayForecast.jsx
@@ -0,0 +1,13 @@
+function FiveDayForcast({ forecast }) {
+  return (
+    <div className="five-day-forecast">
+     <h2>5-Day Forecast</h2>
+      <p>Mon: 21°C</p>
+      <p>Tue: 18°C</p>
+      <p>Wed: 23°C</p>
+      <p>Thu: 19°C</p>
+      <p>Fri: 20°C</p>
+    </div>
+  );
+}
+export default FiveDayForcast;
